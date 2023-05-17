e355a61a123db351539026628432e7a8b4886a30
benberton <91513576+benberton@users.noreply.github.com>
2023-05-17 16:49:39 -0700

    more done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,11 @@
 function unhide(job_ID)
 {
     let element = document.getElementById(job_ID)
-    let caret = element.parentElement.childNodes[1].childNodes[3]
     if (element.classList.contains("hidden"))
     {
-        caret.innerHTML = "&#9650;"
         element.classList.remove("hidden")
     }else
     {
-        caret.innerHTML = "&#9660;"
         element.classList.add("hidden") 
     }
       
